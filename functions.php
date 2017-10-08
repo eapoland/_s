c5b573692fe5f1d9f68260f8bf7b21e104b83908
Marcin Wosinek <marcin.wosinek@gmail.com>
2017-10-08 16:50:05 +0000

    change active menu class

--- functions.php
+++ functions.php
@@ -113,6 +113,16 @@ function efektywnyaltruizm_org_widgets_init() {
 }
 add_action( 'widgets_init', 'efektywnyaltruizm_org_widgets_init' );
 
+add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
+
+function special_nav_class ($classes, $item) {
+  if (in_array('current-menu-item', $classes) ){
+    $classes[] = 'main-menu--active ';
+  }
+
+  return $classes;
+}
+
 /**
  * Enqueue scripts and styles.
  */

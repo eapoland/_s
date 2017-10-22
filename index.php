c73368c3948304c588b3044607ce3d5b57fe662e
Marcin Wosinek <marcin.wosinek@gmail.com>
2017-10-22 11:18:27 +0000

    #8 reuse index form landing-page

--- index.php
+++ index.php
@@ -15,15 +15,12 @@
 get_header(); ?>
 
   <?php
-  if ( have_posts() ) :
 
-    if ( is_home() && ! is_front_page() ) : ?>
-      <header>
-        <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-      </header>
+  if ( is_home() ) :
 
-    <?php
-    endif;
+    include 'landing-page/src/pages/index.html';
+
+  elseif ( have_posts() ) :
 
     /* Start the Loop */
     while ( have_posts() ) : the_post();
